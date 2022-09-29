cba3cdc44514e495ae6f59acc134ea6b87bbab3a
Roderick <m.contactqc@gmail.com>
2022-09-29 12:20:33 +0700

    completed getColors function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-import { GAME_STATUS } from './constants'
+'use strict';
+import { GAME_STATUS, PAIRS_COUNT } from './constants.js';
+import { getRandomColorPairs } from './utils.js';
 
 // Global variables
-let selections = []
-let gameState = GAME_STATUS.PLAYING
+let selections = [];
+let gameState = GAME_STATUS.PLAYING;
 
 // TODOs
 // 1. Generating colors using https://github.com/davidmerfield/randomColor
+getRandomColorPairs(PAIRS_COUNT);
+console.log('colors', getRandomColorPairs(PAIRS_COUNT));
+
 // 2. Attach item click for all li elements
 // 3. Check win logic
 // 4. Add timer
--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,38 @@
+function shuffleColors(colors) {
+  if (!Array.isArray(colors) || colors.length <= 2) return;
+
+  // shuffle colors
+  for (let i = colors.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * i);
+
+    // change color position at index equal i and j
+    const temp = colors[i];
+    colors[i] = colors[j];
+    colors[j] = temp;
+  }
+}
+
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
-}
+
+  const colors = [];
+  const hues = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome'];
+
+  for (let i = 0; i < count; i++) {
+    // Returns a hex code for a 'truly random' color
+    const color = randomColor({
+      luminosity: 'dark',
+      hue: hues[i % hues.length], // if count value > hues length then result is error, so we need to: 7 <= index <= 0
+    });
+
+    // add color to color list
+    colors.push(color);
+  }
+
+  // duplicate color list because the game have 8x2 color
+  const mainColors = [...colors, ...colors];
+  shuffleColors(mainColors);
+
+  return mainColors;
+};
